a14c7484cfb1be4686746a94a8f3fed0174d73d1
Sarvasv-0 <81177923+Sarvasv-0@users.noreply.github.com>
2024-05-14 13:55:26 +0530

    a

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 #include "/home/skyler/.cache/wal/colors-wal-dmenu.h"
 //static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+//	/*     fg         bg       */
 //	[SchemeNorm] = { "#bbbbbb", "#222222" },
 //	[SchemeSel] = { "#eeeeee", "#005577" },
 //	[SchemeOut] = { "#000000", "#00ffff" },
